bc8cad67074418c106ee6f37451e72c6b00fa403
graiola <gennaro.raiola@gmail.com>
2022-12-08 21:29:45 +0100

    Use a service to get the location

---
+++ app/src/main/java/com/schneewittchen/rosandroid/widgets/location/LocationService.java
@@ -0,0 +1,146 @@
+package com.schneewittchen.rosandroid.widgets.location;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import android.Manifest;
+
+import java.util.ArrayList;
+
+public class LocationService extends Service {
+
+    private static final String TAG = LocationService.class.getSimpleName();
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 0;
+    private static final float LOCATION_DISTANCE = 0;
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            Log.i(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+            sendLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.i(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.i(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.i(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate");
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.i(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.i(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listener, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        Log.i(TAG, "initializeLocationManager");
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionsIfNecessary(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                });
+            }
+        }
+    }
+
+    private void sendLocation(Location location) {
+        Intent intent = new Intent();
+        intent.setAction("LOCATION_UPDATE");
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("Location", location);
+        intent.putExtra("Location", bundle);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+    }
+}
--- app/src/main/java/com/schneewittchen/rosandroid/widgets/location/LocationView.java
+++ app/src/main/java/com/schneewittchen/rosandroid/widgets/location/LocationView.java
@@ -1,12 +1,15 @@
 package com.schneewittchen.rosandroid.widgets.location;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -14,18 +17,13 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.schneewittchen.rosandroid.R;
 import com.schneewittchen.rosandroid.ui.views.widgets.PublisherWidgetView;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.util.Log;
-
-import java.util.ArrayList;
 
 
 /**
@@ -40,12 +38,10 @@ public class LocationView extends PublisherWidgetView {
     public static final String TAG = LocationView.class.getSimpleName();
 
     Context context;
-
     Paint buttonPaint;
     TextPaint textPaint;
     StaticLayout staticLayout;
 
-    protected LocationManager locationManager;
     double gpsLatitude;
     double gpsLongitude;
     double gpsAltitude;
@@ -55,25 +51,14 @@ public class LocationView extends PublisherWidgetView {
     long gpsTime = 0;
     long networkTime = 0;
 
-    LocationListener locationListenerGps = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            gpsTime = location.getTime();
-            gpsLatitude = location.getLatitude();
-            gpsLongitude = location.getLongitude();
-            gpsAltitude = location.getAltitude();
-            Log.d("Gps2RosView","GPS - Longitude: "+ gpsLongitude +" Latitude: " + gpsLatitude +" Altitude: " + gpsAltitude);
-            publishCoordinates();
-        }
-    };
-
-    LocationListener locationListenerNetwork = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            networkTime = location.getTime();
-            networkLatitude = location.getLatitude();
-            networkLongitude = location.getLongitude();
-            networkAltitude = location.getAltitude();
-            Log.d("Gps2RosView","NETWORK - Longitude: "+networkLongitude+" Latitude: " + networkLatitude +" Altitude: " + networkAltitude);
-            publishCoordinates();
+    BroadcastReceiver locationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getBundleExtra("Location");
+            Location location = (Location) bundle.getParcelable("Location");
+            if (location != null) {
+                publishCoordinates(location);
+            }
         }
     };
 
@@ -100,37 +85,14 @@ public class LocationView extends PublisherWidgetView {
         textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         textPaint.setTextSize(26 * getResources().getDisplayMetrics().density);
 
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissionsIfNecessary(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            });
-        }
 
-        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        final boolean networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("LOCATION_UPDATE");
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        LocalBroadcastManager.getInstance(context).registerReceiver(locationReceiver, filter);
 
-        if(gpsEnabled)
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
-        if (networkEnabled)
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+        context.startService(new Intent(context, LocationService.class));
 
-        if(!gpsEnabled && !networkEnabled)
-        {
-            Log.e("Gps2RosView","gps and network locations are not enabled!");
-            return;
-        }
-    }
-
-    private void requestPermissionsIfNecessary(String[] permissions) {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this.getContext(), permission)
-                    != PackageManager.PERMISSION_GRANTED) {
-                // Permission is not granted
-                permissionsToRequest.add(permission);
-            }
-        }
     }
 
     private void changeState(boolean pressed) {
@@ -149,10 +111,12 @@ public class LocationView extends PublisherWidgetView {
 
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-                if(entity.buttonPressed)
+                if(entity.buttonPressed) {
                     changeState(false);
-                else
+                }
+                else {
                     changeState(true);
+                }
                 break;
             default:
                 return false;
@@ -194,16 +158,31 @@ public class LocationView extends PublisherWidgetView {
         canvas.restore();
     }
 
-    public void publishCoordinates() {
+    public void publishCoordinates(Location location) {
 
         LocationEntity entity = (LocationEntity) widgetEntity;
 
         if(entity.buttonPressed) {
+
             double latitude;
             double longitude;
             double altitude;
             String type;
-            Log.d("Gps2RosView", "time network " + networkTime + " time GPS "+ gpsTime);
+
+            if(location.getProvider().equals(LocationManager.GPS_PROVIDER))
+            {
+                gpsLatitude = location.getLatitude();
+                gpsLongitude = location.getLongitude();
+                gpsAltitude = location.getAltitude();
+                gpsTime = location.getTime();
+            }
+            else if(location.getProvider().equals(LocationManager.NETWORK_PROVIDER))
+            {
+                networkLatitude = location.getLatitude();
+                networkLongitude = location.getLongitude();
+                networkAltitude = location.getAltitude();
+                networkTime = location.getTime();
+            }
             if( 0 < gpsTime - networkTime) {
                 latitude = gpsLatitude;
                 longitude = gpsLongitude;
@@ -217,9 +196,8 @@ public class LocationView extends PublisherWidgetView {
                 altitude = networkAltitude;
                 type = "NETWORK";
             }
-            Log.d("Gps2RosView", type + " Longitude: " + longitude + " Latitude: " + latitude + " Altitude " + altitude);
+            Log.d(TAG, type + " Longitude: " + longitude + " Latitude: " + latitude + " Altitude " + altitude);
             this.publishViewData(new LocationData(latitude, longitude, altitude, type));
         }
     }
-
 }
